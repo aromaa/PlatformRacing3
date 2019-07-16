3a1513ec1c818fc5026e7dfd116110e42b35c739
isokissa3 <koti94@windowslive.com>
2019-07-16 23:23:21 +0300

    Oh yeah, we had this project too..

--- Server API/Game/Commands/ICommandExecutor.cs
+++ Server API/Game/Commands/ICommandExecutor.cs
@@ -6,6 +6,8 @@ namespace Platform_Racing_3_Server_API.Game.Commands
 {
     public interface ICommandExecutor
     {
+        uint PermissionRank { get; }
+
         void SendMessage(string message);
 
         bool HasPermission(string permission);
