f462408081161731ebd76bcbb932df23f8479d53
isokissa3 <koti94@windowslive.com>
2019-09-27 12:14:21 +0300

    Made Discord bot /pr3parts command to seprate tournament parts and show the amount of parts

---
+++ Common/Extensions/HatExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Platform_Racing_3_Common.Customization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Platform_Racing_3_Common.Extensions
+{
+    public static class HatExtensions
+    {
+        public static bool IsStaffOnly(this Hat hat)
+        {
+            switch(hat)
+            {
+                case Hat.Cowboy:
+                case Hat.Crown:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ Common/Extensions/PartExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Platform_Racing_3_Common.Customization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Platform_Racing_3_Common.Extensions
+{
+    public static class PartExtensions
+    {
+        public static bool IsStaffOnly(this Part part)
+        {
+            switch(part)
+            {
+                case Part.Invisible:
+                case Part.MEME:
+                case Part.Steve:
+                case Part.Rat:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public static bool IsTournamentPrize(this Part part)
+        {
+            switch(part)
+            {
+                case Part.Hoodie:
+                case Part.Cheetah:
+                case Part.Cyborg:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
