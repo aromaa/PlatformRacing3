bf01086b6fa01defd6c2f19d67ac3c43af7c52d6
isokissa3 <koti94@windowslive.com>
2019-12-17 22:56:50 +0200

    Handle Update packet update

--- Server/Game/Communication/Messages/Incoming/Handlers/Match/UpdateIncomingHandler.cs
+++ Server/Game/Communication/Messages/Incoming/Handlers/Match/UpdateIncomingHandler.cs
@@ -3,10 +3,12 @@ using Net.Communication.Incoming.Packet;
 using Net.Communication.Pipeline;
 using Platform_Racing_3_Server.Game.Client;
 using Platform_Racing_3_Server.Game.Communication.Managers;
+using Platform_Racing_3_Server.Game.Communication.Messages.Incoming.Enums;
 using Platform_Racing_3_Server.Game.Communication.Messages.Incoming.Packets.Match;
 using Platform_Racing_3_Server.Game.Match;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Platform_Racing_3_Server.Game.Communication.Messages.Incoming.Handlers.Match
@@ -19,6 +21,101 @@ namespace Platform_Racing_3_Server.Game.Communication.Messages.Incoming.Handlers
             MatchPlayer matchPlayer = session.MultiplayerMatchSession?.MatchPlayer;
             if (matchPlayer != null)
             {
+                if (packet.Status.HasFlag(UpdateStatus.X))
+                {
+                    matchPlayer.X = packet.X;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Y))
+                {
+                    matchPlayer.Y = packet.Y;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.VelX))
+                {
+                    matchPlayer.VelX = packet.VelX;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.VelY))
+                {
+                    matchPlayer.VelY = packet.VelY;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.ScaleX))
+                {
+                    matchPlayer.ScaleX = packet.ScaleX;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Space))
+                {
+                    matchPlayer.Space = packet.Space;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Left))
+                {
+                    matchPlayer.Left = packet.Left;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Right))
+                {
+                    matchPlayer.Right = packet.Right;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Down))
+                {
+                    matchPlayer.Down = packet.Down;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Up))
+                {
+                    matchPlayer.Up = packet.Up;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Speed))
+                {
+                    matchPlayer.Speed = packet.Speed;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Accel))
+                {
+                    matchPlayer.Accel = packet.Accel;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Jump))
+                {
+                    matchPlayer.Jump = packet.Jump;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Rot))
+                {
+                    matchPlayer.Rot = packet.Rotation;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Item))
+                {
+                    matchPlayer.Item = packet.Item;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Life))
+                {
+                    matchPlayer.Life = packet.Life;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Jump))
+                {
+                    matchPlayer.Hurt = packet.Hurt;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Coins))
+                {
+                    matchPlayer.Coins = packet.Coins;
+                }
+
+                if (packet.Status.HasFlag(UpdateStatus.Team))
+                {
+                    matchPlayer.Team = packet.Team;
+                }
+
                 matchPlayer.Match.SendUpdateIfRequired(session, matchPlayer);
             }
         }
