a277604c9ee372bb2d57b48e0c33ed30a59e17a7
Anthony <35195062+BrokenBulb78@users.noreply.github.com>
2020-01-03 00:53:52 +0200

    check if there are any servers

--- Discord Bot/Commands/OnlineCommand.cs
+++ Discord Bot/Commands/OnlineCommand.cs
@@ -21,28 +21,38 @@ namespace Discord_Bot.Commands
         [Summary("Returns server status and active player count.")]
         public Task GetOnlinePlayersCount()
         {
+            EmbedBuilder embed = new EmbedBuilder();
+            
+            embed.WithTitle("Server Status");
+            embed.WithColor(Color.Blue);
+            embed.WithAuthor(author = > { author.WithName(this.Context.User.Username + this.Context.User.Discriminator).WithIconUrl(this.Context.User.GetAvatarUrl()) });
+            
             StringBuilder stringBuilder = new StringBuilder();
+            
+            int serverCount = 0;
 
             foreach (ServerDetails server in this.ServerManager.GetServers())
             {
+                serverCount++;
                 stringBuilder.Append($"**{server.Name}**");
                 stringBuilder.Append(" - ");
                 stringBuilder.Append(server.Status);
                 stringBuilder.AppendLine();
             }
-
-            EmbedBuilder embed = new EmbedBuilder();
-
-            embed.WithTitle("Server Status");
-            embed.WithColor(Color.Blue);
-            embed.WithAuthor(author = > { author.WithName(this.Context.User.Username + this.Context.User.Discriminator).WithIconUrl(this.Context.User.GetAvatarUrl()) });
-
+            
             embed.AddField(new EmbedFieldBuilder()
             {
                 Value = stringBuilder.ToString()
             });
-
-            await this.ReplyAsync(embed: embed.Build());
+            
+            if (serverCount <= 0)
+            {
+                await this.ReplyAsync($"{this.Context.User.Mention} There are no servers! :frowning:");
+            }
+            else
+            {
+                await this.ReplyAsync(embed: embed.Build());
+            }
         }
     }
 }
