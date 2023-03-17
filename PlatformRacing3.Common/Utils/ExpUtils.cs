ef295c6dbb532b6eb9396d38c51122c30c055c27
aromaa <me@joniaromaa.fi>
2023-03-17 17:57:43 +0200

    Use binary search for rank calculation

--- PlatformRacing3.Common/Utils/ExpUtils.cs
+++ PlatformRacing3.Common/Utils/ExpUtils.cs
@@ -1,4 +1,6 @@
-﻿namespace PlatformRacing3.Common.Utils;
+﻿using System.Collections.Immutable;
+
+namespace PlatformRacing3.Common.Utils;
 
 public static class ExpUtils
 {
@@ -13,36 +15,55 @@ public static class ExpUtils
 	public const double REQUIRED_KEY_PRESSES = 60;
 	public const double KEY_PRESSES_MAX_SCALE = 1;
 
-	public static (uint rank, ulong exp) GetRankAndExpFromTotalExp(ulong totalExp)
+	private static readonly ImmutableArray<ulong> ExperienceRequirements = ExpUtils.GenerateExperienceRequirements();
+	private static readonly ImmutableArray<ulong> TotalExperienceRequirements = ExpUtils.GenerateTotalExperienceRequirements();
+
+	private static ImmutableArray<ulong> GenerateExperienceRequirements()
 	{
-		uint currentRank = 0;
-		while (totalExp > 0)
+		static ulong GetExperienceRequirement(uint rank) => rank == 0 ? 1 : checked((ulong)Math.Floor(30 * Math.Pow(1.25, rank - 1)));
+
+		ImmutableArray<ulong>.Builder builder = ImmutableArray.CreateBuilder<ulong>(184 + 1); //184 is the limit until we overflow
+
+		for (uint rank = 0; rank <= 184; rank++)
 		{
-			ulong expNeeded = ExpUtils.GetNextRankExpRequirement(currentRank);
+			builder.Add(GetExperienceRequirement(rank));
+		}
 
-			try
-			{
-				checked
-				{
-					totalExp -= expNeeded;
-					currentRank++;
+		return builder.ToImmutable();
+	}
 
-					if (totalExp == 0)
-					{
-						break;
-					}
-				}
-			}
-			catch(OverflowException)
+	private static ImmutableArray<ulong> GenerateTotalExperienceRequirements()
+	{
+		ImmutableArray<ulong>.Builder builder = ImmutableArray.CreateBuilder<ulong>(178 + 1); //178 is the limit until we overflow
+
+		ulong experience = 0;
+		for (uint rank = 0; rank <= 177; rank++)
+		{
+			builder.Add(experience);
+
+			checked
 			{
-				break;
+				experience += ExpUtils.GetNextRankExpRequirement(rank);
 			}
 		}
 
-		return (currentRank, totalExp);
+		builder.Add(experience);
+
+		return builder.ToImmutable();
 	}
 
-	public static ulong GetNextRankExpRequirement(uint rank) => rank == 0 ? 1 : checked((ulong)Math.Floor(30 * Math.Pow(1.25, rank - 1)));
+	public static (uint Rank, ulong Exp) GetRankAndExpFromTotalExp(ulong totalExp)
+	{
+		int rank = ExpUtils.TotalExperienceRequirements.BinarySearch(totalExp);
+		if (rank < 0)
+		{
+			rank = ~rank - 1;
+		}
+
+		return ((uint)rank, totalExp - ExpUtils.TotalExperienceRequirements[rank]);
+	}
+
+	public static ulong GetNextRankExpRequirement(uint rank) => ExpUtils.ExperienceRequirements[(int)rank];
 
 	public static ulong GetExpEarnedForFinishing(double finishTime) => (ulong)Math.Round(ExpUtils.EXP_FOR_FINISHING * ExpUtils.GetPlaytimeMultiplayer(finishTime));
 	public static ulong GetExpForDefeatingPlayer(uint playerRank) => (ulong)Math.Round(ExpUtils.EXP_BASE_FOR_DEFEATING_PLAYER + (playerRank * ExpUtils.EXP_SCALE_FPR_DEFEATING_PLAYER));
